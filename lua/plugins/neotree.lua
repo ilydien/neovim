ff74f4e0dd617493aa779ba98eae267e650cf51a
Dien <akbardian64@gmail.com>
2026-05-12 10:58:49 +0700

    fix: disable neotree plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,15 +1,23 @@
+-- return {
+--   "nvim-neo-tree/neo-tree.nvim",
+--   opts = {
+--     enable_git_status = true,
+--     window = {
+--       width = 25,
+--     },
+--     filesystem = {
+--       use_libuv_file_watcher = true,
+--       follow_current_file = true,
+--       group_empty_dirs = false,
+--       use_recycle_bin = false,
+--     },
+--   },
+-- }
+--
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    enable_git_status = true,
-    window = {
-      width = 25,
-    },
-    filesystem = {
-      use_libuv_file_watcher = true,
-      follow_current_file = true,
-      group_empty_dirs = false,
-      use_recycle_bin = false,
-    },
+  -- Menonaktifkan file explorer neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
   },
 }
